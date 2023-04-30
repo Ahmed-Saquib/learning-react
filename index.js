bcc5e01a3d39de855a0451d16e992c3c9cb5c52b
Ahmed-Saquib <ahmed.saquib.bd@gmail.com>
2023-04-30 20:00:29 +0600

    User ReactDOM.render() to render an element

---
+++ index.js
@@ -0,0 +1,11 @@
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+// ReactDOM.render() is used to render elements.
+
+/* The first parameter is what do I want to in the screen,
+and the second parameter is where do I want it to be.
+*/
+
+/* Under the hood what react is doing is that it's taking the dom element, 
+we age grabing the getElementById, and its runnig something similar to .append / 
+appendChild to it. In our case its appending our paragraph. 
+*/
