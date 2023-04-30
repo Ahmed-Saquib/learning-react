bb8a76b4456e3a298e141af55bf5454466eebd33
Ahmed-Saquib <ahmed.saquib.bd@gmail.com>
2023-04-30 20:43:19 +0600

    solve quiz

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
-// ReactDOM.render() is used to render elements.
+/*
+Challenge: Starting from scratch, build and render the 
+HTML for our section project. Check the Google slide for 
+what you're trying to build.
 
-/* The first parameter is what do I want to in the screen,
-and the second parameter is where do I want it to be.
-*/
+We'll be adding styling to it later.
 
-/* Under the hood what react is doing is that it's taking the dom element, 
-we age grabing the getElementById, and its runnig something similar to .append / 
-appendChild to it. In our case its appending our paragraph. 
-*/
+Hints:
+* The React logo is a file in the project tree, so you can
+  access it by using `src="./react-logo.png" in your image
+  element
+* You can also set the `width` attribute of the image element
+  just like in HTML. In the slide, I have it set to 40px
+ */
+
+const page = (
+<div>
+    <img src="./react-logo.png" width = "40px"/>
+    <h1>Fun facts about React</h1>
+    <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Is maintained by Facebook</li>
+    </ul>
+</div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
